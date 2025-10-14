1c3e04456d4716392d4bda99b81af4e07ac04c80
scalzone <scalzone@pdx.edu>
2025-10-13 21:59:56 -0700

    fix:center home page logo and adjust header and footer for mobile

--- bar_cart/src/pages/home.jsx
+++ bar_cart/src/pages/home.jsx
@@ -3,11 +3,10 @@ import React, {useState, useEffect} from 'react';
 const Home = () => {
     return (
         <div>
-            <h1 className="header">
-            <img src="/five_o_clock_logo.png" alt="five o'clock logo"/> 
-            </h1>
+            <div className="home-logo-container">
+                <img src="/five_o_clock_logo.png" alt="five o'clock logo" className="home-logo"/> 
+            </div>
         </div>
-      
     );
 }
 export default Home;
